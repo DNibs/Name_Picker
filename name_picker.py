f91bb9321ae2a50c328826df5c8f7161ecf6daa5
DNibs <dnibs513@gmail.com>
2020-03-25 15:06:05 -0400

    reads csv to avoid pandas

--- name_picker.py
+++ name_picker.py
@@ -3,8 +3,9 @@
     Then in the console, type Pyinstaller --onefile draft_order.py
 """
 import random
-import pandas as pd
 import sys
+import csv
+
 
 print("The NEW and IMPROVED Random cadet selector 2.0!")
 fn = input('Type file name:  ')
@@ -12,21 +13,24 @@ if len(fn) == 0:
     print("Need list file name as argument! Terminating...")
     sys.exit()
 
-try:
-    name_df = pd.read_excel(fn, header=None)
-except:
-    print("File not found! Terminating...")
-    sys.exit()
-name_list = name_df.values.tolist()
+name_list = []
 
+with open(fn) as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    for row in csv_reader:
+        name_list.append(row[0])
+
+name_list.pop(0)
 if len(name_list) == 0:
     print("Empty list! Terminating...")
     sys.exit()
+print(name_list)
+print('\n')
 
 x = input('Press q to quit, any other to select a cadet: ')
 while x != 'q':
     picked_num = random.randint(0, len(name_list) - 1)
     print('And the lucky cadet is...')
-    print(name_list[picked_num][0])
+    print(name_list[picked_num])
     print('')
     x = input('Press q to quit, any other to select a cadet: ')
